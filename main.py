a68d7200b961cdaaa494d3b28b3d6fab2c5a77d7
Nicholas Lee <nicholasl3688@gmail.com>
2026-03-25 15:27:48 -0400

    CSV to df

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-print("Hello, World!")
+consumer_confidence_df = pd.read_csv("Consumer Confidence.csv")
+delinquency_df = pd.read_csv("Deliquency.csv")
+housing_price_index_df = pd.read_csv("Housing Price Index.csv")
+unemployment_rate_df = pd.read_csv("Unemployment Rate.csv")
+zillow_housing_df = pd.read_csv("Zillow Housing Data.csv")
+
