1577951bb77683e5933f0d70cef772149452a242
Nicholas Lee <nicholasl3688@gmail.com>
2026-03-25 15:15:58 -0400

    test3

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+
+
+
+print("asdsafdsafdsafdsafdsaf")
